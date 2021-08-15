f4121af3475380a0295ca088749ea738327bda01
Shamel98 <shameljohn98@gmail.com>
2021-08-15 19:09:57 +0530

    End of Section 14

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -56,7 +56,7 @@ namespace API.Controllers
 
         public ActionResult<string> GetBadRequest()
         {
-            return BadRequest("This was not a good request");
+            return BadRequest();
 
            
         }
---
+++ API/Entities/UserLike.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Entities
+{
+    public class UserLike
+    {
+        public AppUser SourceUser { get; set; }
+
+        public int SourceUserId { get; set; }
+
+        public AppUser LikedUser { get; set; }
+
+        public int LikedUserId { get; set; }
+        public int LikedUsersId { get; internal set; }
+        public AppUser LikedUsers { get; internal set; }
+    }
+}
